60b5352f7639369ed9ff33edb49354cb4e1b0c41
Tsuki <i@sukitsuki.com>
2017-04-02 06:06:33 +0800

    add vertx

--- src/main/java/org/yanex/telegram/TelegramBot.kt
+++ src/main/java/org/yanex/telegram/TelegramBot.kt
@@ -1,25 +1,24 @@
 package org.yanex.telegram
 
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import okhttp3.MediaType
 import okhttp3.OkHttpClient
-import okhttp3.RequestBody
 import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
 import org.yanex.telegram.handler.StopProcessingException
 import org.yanex.telegram.handler.UpdateHandler
-import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+
 class TelegramBot internal constructor(
         serviceProvider: TelegramBotService,
         val timeout: Int = 30
-) : TelegramBotService by serviceProvider {
+                                      ) : TelegramBotService by serviceProvider {
     companion object {
         @JvmStatic
-        fun create(token: String, timeout: Int = 30, logLevel: HttpLoggingInterceptor.Level): TelegramBot {
+        fun create(token: String, timeout: Int = 30, logLevel: Level = BASIC): TelegramBot {
             val logging = HttpLoggingInterceptor().apply {
                 level = logLevel
             }
@@ -30,15 +29,12 @@ class TelegramBot internal constructor(
                     .readTimeout(timeout + 10L, TimeUnit.SECONDS)
                     .writeTimeout(timeout + 10L, TimeUnit.SECONDS)
                     .build()
-            
-            val gson = Gson()
-            
             val adapter = Retrofit.Builder()
                     .baseUrl("https://api.telegram.org/bot$token/")
-                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addConverterFactory(GsonConverterFactory.create(Gson()))
                     .client(httpClient)
                     .build()
-            
+
             return TelegramBot(adapter.create(TelegramBotService::class.java), timeout)
         }
     }
@@ -46,17 +42,17 @@ class TelegramBot internal constructor(
     fun listen(maxId: Long, handler: UpdateHandler): Long {
         var currentMaxId: Long = maxId
         outer@ while (true) {
-            val call = if (currentMaxId > 0) 
+            val call = if (currentMaxId > 0)
                 getUpdates(offset = currentMaxId, timeout = timeout)
-            else 
+            else
                 getUpdates(timeout = timeout)
-            
+
             val response = call.execute()
-            
+
             //TODO can we do anything with this?
             if (!response.isSuccessful) continue
             val responseBody = response.body() ?: continue
-            
+
             val updates = responseBody.result ?: if (handler.onError(responseBody)) continue else break
             for (update in updates) {
                 try {
